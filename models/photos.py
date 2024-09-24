23cc56ea9db107b84eecae81ed3025a435a385f4
George K (MHG) <george.khananaev@gmail.com>
2024-09-24 20:31:18 +0700

    changed the models slightly

--- models/photos.py
+++ models/photos.py
@@ -3,23 +3,23 @@ from pydantic import BaseModel
 
 
 class Tag(BaseModel):
-    id: Optional[int]
-    tag: Optional[str]
+    id: Optional[int] = None
+    tag: Optional[str] = None
 
 
 class MLTag(BaseModel):
-    confidence: Optional[int]
-    tag_type: Optional[str]
-    tag_id: Optional[int]
-    photo_id: Optional[int]
-    tag_name: Optional[str]
+    confidence: Optional[int] = None
+    tag_type: Optional[str] = None
+    tag_id: Optional[int] = None
+    photo_id: Optional[int] = None
+    tag_name: Optional[str] = None
 
 
 class Photo(BaseModel):
-    descriptiontype_id: Optional[int]
+    descriptiontype_id: Optional[int] = None
     ml_tags: Optional[List[MLTag]] = []
-    photo_id: Optional[int]
+    photo_id: Optional[int] = None
     tags: Optional[List[Tag]] = []
-    url_square60: Optional[str]
-    url_max: Optional[str]
-    url_1440: Optional[str]
+    url_square60: Optional[str] = None
+    url_max: Optional[str] = None
+    url_1440: Optional[str] = None
--- models/rooms.py
+++ models/rooms.py
@@ -3,107 +3,107 @@ from pydantic import BaseModel, validator # noqa
 
 
 class BMaxLosData(BaseModel):
-    is_fullon: Optional[int]
-    extended_los: Optional[int]
-    max_allowed_los: Optional[int]
-    experiment: Optional[str]
-    has_extended_los: Optional[int]
-    default_los: Optional[int]
+    is_fullon: Optional[int] = None
+    extended_los: Optional[int] = None
+    max_allowed_los: Optional[int] = None
+    experiment: Optional[str] = None
+    has_extended_los: Optional[int] = None
+    default_los: Optional[int] = None
 
 
 class Facility(BaseModel):
-    facilitytype_id: Optional[int]
-    id: Optional[int]
-    name: Optional[str]
-    alt_facilitytype_id: Optional[int]
-    alt_facilitytype_name: Optional[str]
+    facilitytype_id: Optional[int] = None
+    id: Optional[int] = None
+    name: Optional[str] = None
+    alt_facilitytype_id: Optional[int] = None
+    alt_facilitytype_name: Optional[str] = None
 
 
 class Highlight(BaseModel):
-    translated_name: Optional[str]
-    icon: Optional[str]
-    id: Optional[Union[str, int]]  # Make this field optional
+    translated_name: Optional[str] = None
+    icon: Optional[str] = None
+    id: Optional[Union[str, int]] = None  # Make this field optional
 
 
 class BedType(BaseModel):
-    name_with_count: Optional[str]
-    count: Optional[int]
-    description_localized: Optional[str]  # Optional field
-    description_imperial: Optional[str]
-    name: Optional[str]
-    bed_type: Optional[int]
-    description: Optional[str]
+    name_with_count: Optional[str] = None
+    count: Optional[int] = None
+    description_localized: Optional[str] = None  # Optional field
+    description_imperial: Optional[str] = None
+    name: Optional[str] = None
+    bed_type: Optional[int] = None
+    description: Optional[str] = None
 
 
 class BedConfiguration(BaseModel):
-    bed_types: Optional[List[BedType]]
+    bed_types: Optional[List[BedType]] = None
 
 
 class Photo(BaseModel):
-    photo_id: Optional[Union[str, int]]  # Allow either string or integer
-    ratio: Optional[Union[str, float]]  # Allow either string or float
-    new_order: Optional[Union[str, int]]  # Allow either string or integer
+    photo_id: Optional[Union[str, int]] = None  # Allow either string or integer
+    ratio: Optional[Union[str, float]] = None  # Allow either string or float
+    new_order: Optional[Union[str, int]] = None  # Allow either string or integer
 
 
 class Room(BaseModel):
-    facilities: Optional[List[Facility]]
-    description: Optional[str]
+    facilities: Optional[List[Facility]] = None
+    description: Optional[str] = None
     highlights: Optional[List[Highlight]]  # Optional highlights list
-    bed_configurations: Optional[List[BedConfiguration]]
-    private_bathroom_count: Optional[int]
-    children_and_beds_text: Optional[Dict]
-    is_high_floor_guaranteed: Optional[int]
-    private_bathroom_highlight: Optional[Dict]
-    photos: Optional[List[Photo]]
+    bed_configurations: Optional[List[BedConfiguration]] = None
+    private_bathroom_count: Optional[int] = None
+    children_and_beds_text: Optional[Dict] = None
+    is_high_floor_guaranteed: Optional[int] = None
+    private_bathroom_highlight: Optional[Dict] = None
+    photos: Optional[List[Photo]] = None
 
 
 class MinRoomDistribution(BaseModel):
-    children: Optional[List[int]]
-    adults: Optional[int]
+    children: Optional[List[int]] = None
+    adults: Optional[int] = None
 
 
 class RoomRecommendation(BaseModel):
-    total_extra_bed_price: Optional[int]
-    children: Optional[int]
-    block_id: Optional[str]
-    number_of_extra_beds: Optional[int]
-    babies: Optional[int]
-    adults: Optional[int]
-    total_extra_bed_price_in_hotel_currency: Optional[int]
+    total_extra_bed_price: Optional[int] = None
+    children: Optional[int] = None
+    block_id: Optional[str] = None
+    number_of_extra_beds: Optional[int] = None
+    babies: Optional[int] = None
+    adults: Optional[int] = None
+    total_extra_bed_price_in_hotel_currency: Optional[int] = None
 
 
 class RoomsData(BaseModel):
     block: Optional[Any] = []
-    b_max_los_data: Optional[BMaxLosData]
+    b_max_los_data: Optional[BMaxLosData] = None
     rooms: Optional[Any] = []
-    preferences: Optional[List[str]]
-    recommended_block_title: Optional[str]
+    preferences: Optional[List[str]] = []
+    recommended_block_title: Optional[str] = None
     hotel_id: Optional[int]
-    qualifies_for_no_cc_reservation: Optional[int]
-    address_required: Optional[int]
-    min_room_distribution: Optional[MinRoomDistribution]
-    cvc_required: Optional[str]
-    is_cpv2_property: Optional[int]
-    cheapest_avail_price_eur: Optional[float]
-    room_recommendation: Optional[List[RoomRecommendation]]
-    currency_code: Optional[str]
-    b_blackout_android_prepayment_copy: Optional[int]
-    departure_date: Optional[str]
-    arrival_date: Optional[str]
-    prepayment_policies: Optional[Dict[str, Optional[str]]]
+    qualifies_for_no_cc_reservation: Optional[int] = None
+    address_required: Optional[int] = None
+    min_room_distribution: Optional[MinRoomDistribution] = None
+    cvc_required: Optional[str] = None
+    is_cpv2_property: Optional[int] = None
+    cheapest_avail_price_eur: Optional[float] = None
+    room_recommendation: Optional[List[RoomRecommendation]] = []
+    currency_code: Optional[str] = None
+    b_blackout_android_prepayment_copy: Optional[int] = None
+    departure_date: Optional[str] = None
+    arrival_date: Optional[str] = None
+    prepayment_policies: Optional[Any] = None
     direct_payment: Optional[Any] = None
-    cancellation_policies: Optional[Dict[str, Optional[str]]]
-    cc_required: Optional[str]
-    payment_detail: Optional[Dict[str, Optional[str]]]
-    max_rooms_in_reservation: Optional[int]
-    is_exclusive: Optional[int]
-    total_blocks: Optional[int]
-    duplicate_rates_removed: Optional[int]
-    top_ufi_benefits: Optional[List[Dict[str, Optional[str]]]]
-    last_matching_block_index: Optional[int]
-    soldout_rooms: Optional[List[str]]
-    tax_exceptions: Optional[List[str]]
-    preferences: Optional[List[Union[str, Dict]]]
+    cancellation_policies: Optional[Any] = None
+    cc_required: Optional[str] = None
+    payment_detail: Optional[Any] = None
+    max_rooms_in_reservation: Optional[int] = None
+    is_exclusive: Optional[int] = None
+    total_blocks: Optional[int] = None
+    duplicate_rates_removed: Optional[int] = None
+    top_ufi_benefits: Optional[Any] = None
+    last_matching_block_index: Optional[int] = None
+    soldout_rooms: Optional[List[str]] = []
+    tax_exceptions: Optional[List[str]] = []
+    preferences: Optional[Any] = None
 
     # Validator for the 'preferences' field
     @validator("preferences", pre=True, always=True) # noqa
